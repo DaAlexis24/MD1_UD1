d1b97138c64fd1b3889e30e7522feb3469ba4ec9
danielSoledad <danielalesole24@gmail.com>
2024-11-27 11:36:29 +0100

    Update info

---
+++ index.js
@@ -0,0 +1,10 @@
+function handleSubmit(ev) {
+    ev.preventDefault();
+    console.log(ev);
+    const elements = ev.target.elements;
+    console.log(elements.namedItem("firstname").value);
+}
+
+const form = document.querySelector("form");
+console.dir(form);
+// form.addEventListener("submit", handleSubmit);
